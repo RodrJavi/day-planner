83641445403b4cea5fb9b70414437b315f7cb7fe
Javi <nb2javi@gmail.com>
2023-07-16 22:13:02 -0500

    added event listeners to buttons and dynamically displayed descriptions for each hour of the day

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,33 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const hourListHour = $("#hourList").children().data("hour");
+const hourListText = $("#hourList").children().children("textarea");
+const hourListDiv = $("#hourList").children();
+const saveButton = $(".saveBtn");
 $(function () {
+  // Sets date on header to current day
   $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  hourListDiv.each(function () {
+    //Checks if there is a description saved in localstorage corresponding to the hour
+    // If yes, changes description to the value
+    if (localStorage.getItem($(this).data("hour"))) {
+      $(this)
+        .children(".description")
+        .val(localStorage.getItem($(this).data("hour")));
+    }
+    // Determines color of div based on hour of the day
+    if ($(this).data("hour") > dayjs().hour()) {
+      $(this).children().addClass("future");
+    } else if ($(this).data("hour") < dayjs().hour()) {
+      $(this).children().addClass("past");
+    } else {
+      $(this).children().addClass("present");
+    }
+  });
+
+  // Adds event listener to each button to save whatever is in description field to localstorage
+  saveButton.on("click", (e) => {
+    let thisHour = $(e.currentTarget).parent().data("hour");
+    let thisDesc = $(e.currentTarget).siblings(".description").val();
+    localStorage.setItem(thisHour, thisDesc);
+  });
 });
